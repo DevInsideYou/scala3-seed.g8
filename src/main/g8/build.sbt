a03319dbc81a40803adc0324c446ded77b04c653
Vladyslav Pekker <agilesteel@gmail.com>
2021-02-25 11:04:35 +0400

    Scala v3.0.0-RC1

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,7 +1,7 @@
 import Dependencies._
 
 ThisBuild / organization := "$organization;format="lower,package"$"
-ThisBuild / scalaVersion := "3.0.0-M3"
+ThisBuild / scalaVersion := "3.0.0-RC1"
 
 ThisBuild / scalacOptions ++=
   Seq("-rewrite", "-indent") ++ Seq(
--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,12 +4,12 @@ object Dependencies {
   case object org {
     case object scalatest {
       val scalatest =
-        "org.scalatest" %% "scalatest" % "3.2.3"
+        "org.scalatest" %% "scalatest" % "3.2.5"
     }
 
     case object scalatestplus {
       val `scalacheck-1-15` =
-        "org.scalatestplus" %% "scalacheck-1-15" % "3.2.3.0"
+        "org.scalatestplus" %% "scalacheck-1-15" % "3.2.5.0"
     }
   }
 }
