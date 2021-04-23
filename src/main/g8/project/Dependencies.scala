5c9f2b5621d4f4b4cc7d81166bb16ad920b2a07a
Vladyslav Pekker <agilesteel@gmail.com>
2021-04-23 14:44:57 +0400

    scala v3.0.0-RC2 -> v3.0.0-RC3

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,12 +4,12 @@ object Dependencies {
   case object org {
     case object scalatest {
       val scalatest =
-        "org.scalatest" %% "scalatest" % "3.2.7"
+        "org.scalatest" %% "scalatest" % "3.2.8"
     }
 
     case object scalatestplus {
       val `scalacheck-1-15` =
-        "org.scalatestplus" %% "scalacheck-1-15" % "3.2.7.0"
+        "org.scalatestplus" %% "scalacheck-1-15" % "3.2.8.0"
     }
   }
 }
