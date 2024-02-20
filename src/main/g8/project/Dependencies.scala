b95f2b7fa7647f52b008078847518792b80b68e7
Vladyslav Pekker <agilesteel@gmail.com>
2024-02-20 20:15:50 -0300

    housekeeping

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
         moduleId("munit-scalacheck")
 
       private def moduleId(artifact: String): ModuleID =
-        "org.scalameta" %% artifact % "1.0.0-M10"
+        "org.scalameta" %% artifact % "1.0.0-M11"
     }
 
     object typelevel {
